1f537cb8abf69d929a059b6660b1287c1a368356
Alexander <protonull@protonmail.com>
2023-12-18 15:47:17 +0000

    Remove Lombok :(

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -9,7 +9,4 @@ dependencies {
 	compileOnly("net.civmc.namelayer:namelayer-paper:3.0.4:dev")
 	compileOnly("net.civmc.citadel:citadel-paper:5.1.2:dev")
 	compileOnly("net.civmc.jukealert:jukealert-paper:3.0.7:dev")
-
-	compileOnly("org.projectlombok:lombok:1.18.24")
-	annotationProcessor("org.projectlombok:lombok:1.18.24")
 }
